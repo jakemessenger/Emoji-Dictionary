875b50698fcd6793773fb7ffccc5bf7af97f3ba6
Jake Messenger <jake.messenger@mac.com>
2017-03-26 14:38:30 +0100

    Working on second view

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
+// create the View Controller. TableView is called "emojiTable"
+
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var emojiTable: UITableView!
     
-    var emojis = ["😃","🤓","🤠","😩","🤔","💩","🎅🏻"]
+    // create the Array called "emojis"
+    
+    var emojis = ["😃","🤓","🤠","😩","🤔","💩","🎅🏻","😻"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +25,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         emojiTable.dataSource = self
         emojiTable.delegate = self
     }
+    // count the number of items in the array
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return emojis.count
@@ -29,6 +34,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         print(indexPath.row)
         let cell = UITableViewCell()
+        // fill the positions in the table view with the corresponding positions in the array
         cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
